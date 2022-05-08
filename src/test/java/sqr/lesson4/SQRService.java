d9f1ac69945bd3320e3667bbffc26b2b5468f855
Eugene <korazon2271@gmail.com>
2022-05-08 20:35:25 +0300

    Исправление названия переменной

--- src/test/java/sqr/lesson4/SQRService.java
+++ src/test/java/sqr/lesson4/SQRService.java
@@ -3,20 +3,20 @@ package sqr.lesson4;
 public class SQRService {
 
     public int calculateSqrNumberInBorders(int lowerBorder, int upperBorder){
-        int SqrNumber = 0;
+        int sqrNumber = 0;
 
         for (int i = 0;i< upperBorder;i++) {
                 if (i*i >= lowerBorder && i*i <= upperBorder){
-                    SqrNumber = SqrNumber+1;
+                    sqrNumber = sqrNumber+1;
                 }
 
             }
         if(lowerBorder == 0 && upperBorder <= 1){
-            SqrNumber = SqrNumber+1;
+            sqrNumber = sqrNumber+1;
         }
         if(lowerBorder == 1 && upperBorder == 1){
-            SqrNumber = SqrNumber+1;
+            sqrNumber = sqrNumber+1;
         }
-        return SqrNumber;
+        return sqrNumber;
     }
 }
